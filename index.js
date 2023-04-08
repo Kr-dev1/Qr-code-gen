83b3622a9c55cbf5ceb28cc4322dec5501e2376b
KasturiRangan <105440209+SKasturirangan@users.noreply.github.com>
2023-04-08 13:58:59 +0530

    Completed

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ const qrContainer = document.querySelector(".qr-body");
 let size = sizes.value;
 generateBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  generateQRCode();
+  if (qrText.value.length > 0) {
+    generateQRCode();
+  } else {
+    alert("Enter the text or URL to generate QRCode");
+  }
+});
+
+sizes.addEventListener("change", (e) => {
+  size = e.target.value;
+  if (qrText.value.length > 0) {
+    generateQRCode();
+  } else {
+    alert("Enter the text or URL to generate QRCode");
+  }
 });
 
 function generateQRCode() {
@@ -20,3 +33,16 @@ function generateQRCode() {
     colorDark: "#000",
   });
 }
+
+downloadBtn.addEventListener("click", () => {
+  let img = document.querySelector(".qr-body img");
+  if (img !== null) {
+    let imgAtrr = img.getAttribute("src");
+    downloadBtn.setAttribute("href", imgAtrr);
+  } else {
+    downloadBtn.setAttribute(
+      "href",
+      `${document.querySelector("canvas").toDataURL()}`
+    );
+  }
+});
